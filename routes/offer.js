8fc22163589c066975cd0198e3f18481d22a68ee
WalidBoussaid <walidmeknessi@hotmail.com>
2022-09-09 18:09:09 +0200

    ajout du router updateOffer + deleteOffer coté employeur

--- routes/offer.js
+++ routes/offer.js
@@ -137,9 +137,119 @@ router.get("/myOffer/:id", passport, async (req, res) => {
 });
 
 //route qui update l'offre de l'empoyeur
-// router.post("/updateMyOffer", passport, async (req, res) => {
+router.post("/updateMyOffer/:id", passport, async (req, res) => {
+    try {
+        const title = req.body.title;
+        const description = req.body.description;
+        const categoryId = req.body.categoryId;
+        const typeOfferId = req.body.typeOfferId;
 
-// });
+        const offerId = req.params.id;
+        const userId = req.user.userId;
+
+        const offer = await Offer.findOne({
+            where: {
+                id: offerId,
+                employerId: userId,
+            },
+            include: [
+                {
+                    model: CategoryJob,
+                },
+                {
+                    model: TypeOffer,
+                },
+            ],
+        });
+
+        const category = await CategoryJob.findOne({
+            where: {
+                id: categoryId,
+            },
+        });
+
+        const type = await TypeOffer.findOne({
+            where: {
+                id: typeOfferId,
+            },
+        });
+
+        if (offer == null || offer == "") {
+            res.status(404).json({ err: "Il n'y a pas d'offre trouver !" });
+        }
+        if (category == null || category == "") {
+            res.status(404).json({
+                err: "Il n'y a pas de categorie trouver !",
+            });
+        }
+        if (type == null || type == "") {
+            res.status(404).json({
+                err: "Il n'y a pas de type d'offre trouver !",
+            });
+        }
+        if (categoryId == null || categoryId == "") {
+            res.status(404).json({ err: "La categorie n'esxiste pas !" });
+        }
+        if (typeOfferId == null || typeOfferId == "") {
+            res.status(404).json({ err: "Le type d'offre n'esxiste pas !" });
+        }
+        if (title.length < 4) {
+            res.status(404).json({
+                err: "Titre pas confomre (min 4 caractères)!",
+            });
+        }
+        if (description.length < 10) {
+            res.status(404).json({
+                err: "Description pas confomre (min 10 caractères)!",
+            });
+        }
+
+        //Modification valeurs des colonnes title et description de l'objet Offer
+        offer.set({
+            title: title,
+            description: description,
+        });
+
+        await offer.save(); //save l'objet offer avec les nouvelles valeurs des colonnes title et description
+
+        offer.setCategoryJob(category);
+        offer.setTypeOffer(type);
+
+        return res.json(true);
+    } catch (error) {
+        res.status(404).json(error.message);
+    }
+});
+
+//route qui supprime l'offre de l'employeur
+router.delete("/deleteOffer/:id", passport, async (req, res) => {
+    try {
+        const offerId = req.params.id;
+        const userId = req.user.userId;
+
+        const offer = await Offer.findOne({
+            where: {
+                id: offerId,
+                employerId: userId,
+            },
+            include: [
+                {
+                    model: CategoryJob,
+                },
+                {
+                    model: TypeOffer,
+                },
+            ],
+        });
+        if (offer) {
+            await offer.destroy(); //fonction qui suprime une offre
+        } else {
+            res.status(404).json({ err: "L'offre n'existe pas !" });
+        }
+
+        return res.json(true);
+    } catch (error) {}
+});
 
 //route qui permet au candidat de de consulter toute les offres(hors filtre)
 router.get("/AllOffer", async (req, res) => {
